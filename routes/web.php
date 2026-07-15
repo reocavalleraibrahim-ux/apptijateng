8c868e790b306e2f91885c1c1d8fadf594011f9b
reocavallera <reocavalleraibrahim@gmail.com>
2026-07-15 15:28:04 +0700

    awal

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/login',[AuthController::class,'login']);
+Route::post('/authenticate',[AuthController::class,'authenticate']);
+
